1b4f13d2ee2a3bd69effa497ec1765e015f78571
Bagrat Hakobyan <b.a.hakobyan@gmail.com>
2021-05-10 17:41:00 +0400

    `Process` component added

---
+++ src/Tier/BusinessLogic/Infrastructure/Process.php
@@ -0,0 +1,70 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the Digital Foundation packages.
+ *
+ * (c) Bagrat Hakobyan <b.a.hakobyan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DigitalFoundation\UtilBundle\BusinessLogic\Infrastructure;
+
+
+/**
+ * Class Process
+ *
+ * @author Bagrat Hakobyan <b.a.hakobyan@gmail.com>
+ *
+ * @package DigitalFoundation\UtilBundle\BusinessLogic\Infrastructure
+ */
+abstract class Process {
+
+    /**
+     * @param int $value
+     *
+     * @return void
+     */
+    static public function setMemoryLimit(int $value): void {
+        ini_set("memory_limit", "{$value}M");
+    }
+
+    /**
+     * @param int $seconds
+     *
+     * @return void
+     */
+    static public function setTimeLimit(int $seconds): void {
+        set_time_limit($seconds);
+    }
+
+    /**
+     * @param int $memoryLimit
+     *
+     * @return void
+     */
+    static public function initBigProcess(int $memoryLimit = 4096): void {
+        self::setMemoryLimit($memoryLimit);
+    }
+
+    /**
+     * @param int $timeLimit
+     *
+     * @return void
+     */
+    static public function initLongProcess(int $timeLimit = 0): void {
+        self::setTimeLimit($timeLimit);
+    }
+
+    /**
+     * @param int $memoryLimit
+     * @param int $timeLimit
+     *
+     * @return void
+     */
+    static public function initBigLongProcess(int $memoryLimit = 4096, int $timeLimit = 0): void {
+        self::initBigProcess($memoryLimit);
+        self::initLongProcess($timeLimit);
+    }
+}
